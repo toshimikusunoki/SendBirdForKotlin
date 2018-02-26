e684dc21ff4872f2e72834a64e4c92d5eb80aa72
toshimi kusunoki <toshimikusunoki@gmail.com>
2018-02-26 21:33:05 +0900

    参加者一覧表示機能追加

---
+++ app/src/main/java/com/sendbird/android/sample/groupchannel/UserListAdapter.kt
@@ -0,0 +1,90 @@
+package com.sendbird.android.sample.groupchannel
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sendbird.android.Member
+import com.sendbird.android.SendBird
+import com.sendbird.android.User
+import com.sendbird.android.sample.R
+import com.sendbird.android.sample.utils.ImageUtils
+import kotlinx.android.synthetic.main.list_item_user.view.*
+import java.util.*
+
+/**
+ * A simple adapter that displays a list of Users.
+ */
+class UserListAdapter(private val mContext: Context, private val mChannelUrl: String, private val mIsGroupChannel: Boolean) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val mUsers: MutableList<User>
+
+    init {
+        mUsers = ArrayList()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_user, parent, false)
+        return UserHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as UserHolder).bind(mContext, holder, mUsers[position])
+    }
+
+    override fun getItemCount(): Int {
+        return mUsers.size
+    }
+
+    fun setUserList(users: List<User>) {
+        mUsers.clear()
+        mUsers.addAll(users)
+        notifyDataSetChanged()
+    }
+
+    fun addLast(user: User) {
+        mUsers.add(user)
+        notifyDataSetChanged()
+    }
+
+    private inner class UserHolder internal constructor(private val view: View) : RecyclerView.ViewHolder(view) {
+
+        internal fun bind(context: Context, holder: UserHolder, user: User) {
+            view.text_user_list_nickname.text = user.nickname
+            ImageUtils.displayRoundImageFromUrl(context, user.profileUrl, view.image_user_list_profile)
+
+            if (mIsGroupChannel) {
+                if (SendBird.getCurrentUser().userId == user.userId) {
+                    view.relative_layout_blocked_by_me.visibility = View.GONE
+                    view.text_view_blocked.visibility = View.GONE
+                } else {
+                    view.relative_layout_blocked_by_me.visibility = View.VISIBLE
+
+                    holder.view.setOnClickListener {
+                        // TODO:
+//                        val intent = Intent(context, MemberInfoActivity::class.java)
+//                        intent.putExtra(MemberListActivity.EXTRA_CHANNEL_URL, mChannelUrl)
+//                        intent.putExtra(MemberListActivity.EXTRA_USER_ID, user.userId)
+//                        intent.putExtra(MemberListActivity.EXTRA_USER_PROFILE_URL, user.profileUrl)
+//                        intent.putExtra(MemberListActivity.EXTRA_USER_NICKNAME, user.nickname)
+//                        intent.putExtra(MemberListActivity.EXTRA_USER_BLOCKED_BY_ME, (user as Member).isBlockedByMe)
+//                        context.startActivity(intent)
+                    }
+                }
+
+                val isBlockedByMe = (user as Member).isBlockedByMe
+                if (isBlockedByMe) {
+                    view.image_user_list_blocked.visibility = View.VISIBLE
+                    view.text_view_blocked.visibility = View.VISIBLE
+                } else {
+                    view.image_user_list_blocked.visibility = View.GONE
+                    view.text_view_blocked.visibility = View.GONE
+                }
+            } else {
+                view.image_user_list_blocked.visibility = View.GONE
+                view.relative_layout_blocked_by_me.visibility = View.GONE
+            }
+        }
+    }
+}
+
--- app/src/main/java/com/sendbird/android/sample/openchannel/ParticipantListActivity.kt
+++ app/src/main/java/com/sendbird/android/sample/openchannel/ParticipantListActivity.kt
@@ -2,12 +2,105 @@ package com.sendbird.android.sample.openchannel
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import com.sendbird.android.OpenChannel
+import com.sendbird.android.SendBird
+import com.sendbird.android.User
+import com.sendbird.android.UserListQuery
 import com.sendbird.android.sample.R
+import com.sendbird.android.sample.groupchannel.UserListAdapter
+import kotlinx.android.synthetic.main.activity_participant_list.*
+import java.util.*
+
+/**
+ * Displays a list of the participants of a specified Open Channel.
+ */
 
 class ParticipantListActivity : AppCompatActivity() {
 
+    private var mListAdapter: UserListAdapter? = null
+    private var mLayoutManager: LinearLayoutManager? = null
+    private var mChannel: OpenChannel? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_participant_list)
+
+        val mChannelUrl = intent.getStringExtra(OpenChatFragment.EXTRA_CHANNEL_URL)
+        mListAdapter = UserListAdapter(this, mChannelUrl, false)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar_participant_list)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_left_white_24_dp)
+
+        setUpRecyclerView()
+
+        getChannelFromUrl(mChannelUrl)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == android.R.id.home) {
+            onBackPressed()
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setUpRecyclerView() {
+        mLayoutManager = LinearLayoutManager(this)
+        recycler_participant_list.layoutManager = mLayoutManager
+        recycler_participant_list.adapter = mListAdapter
+        recycler_participant_list.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+    }
+
+    private fun getChannelFromUrl(url: String) {
+        OpenChannel.getChannel(url, OpenChannel.OpenChannelGetHandler { openChannel, e ->
+            if (e != null) {
+                // Error!
+                return@OpenChannelGetHandler
+            }
+
+            mChannel = openChannel
+
+            getUserList()
+        })
+    }
+
+    private fun getUserList() {
+        val userListQuery = mChannel!!.createParticipantListQuery()
+        userListQuery.next(UserListQuery.UserListQueryResultHandler { list, e ->
+            if (e != null) {
+                // Error!
+                return@UserListQueryResultHandler
+            }
+
+            setUserList(list)
+        })
+    }
+
+    private fun setUserList(userList: List<User>) {
+        val sortedUserList = ArrayList<User>()
+        for (me in userList) {
+            if (me.userId == SendBird.getCurrentUser().userId) {
+                sortedUserList.add(me)
+                break
+            }
+        }
+        for (other in userList) {
+            if (other.userId == SendBird.getCurrentUser().userId) {
+                continue
+            }
+            sortedUserList.add(other)
+        }
+
+        mListAdapter!!.setUserList(sortedUserList)
     }
 }
